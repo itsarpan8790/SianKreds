2b13ca7b6b338d1eea5202a6e532002a1265ca5a
itsarpan8790 <arpank6@gmail.com>
2023-12-08 10:12:16 +0530

    updated

--- Com.SianKred/src/test/java/javaAssignments/Q1_i_RemDup_Occ_Unique_Dup_COLLECTION_7Dec.java
+++ Com.SianKred/src/test/java/javaAssignments/Q1_i_RemDup_Occ_Unique_Dup_COLLECTION_7Dec.java
@@ -7,33 +7,33 @@ public class Q1_i_RemDup_Occ_Unique_Dup_COLLECTION_7Dec {
 		int[] a = { 5, 4, 4, 2, 5, 4, 2, 1 };
 
 		LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
-        //Step 1.Adding to set to Remove Duplicates.
+		// Step 1.Adding to set to Remove Duplicates.
 		for (int i = 0; i < a.length; i++) {
 			set.add(a[i]);
 
 		}
-		//Printing Set
-		for(Integer s:set) {
-			System.out.print(s+" ");
+		// Printing Set
+		for (Integer s : set) {
+			System.out.print(s + " ");
 		}
 		System.out.println("");
-		
-		//Step 2.Checking Occurence
-		for(Integer s:set) {
-			int count=0;
-			for(int i=0;i<a.length;i++) {
-				if(s==a[i]) {
+
+		// Step 2.Checking Occurence
+		for (Integer s : set) {
+			int count = 0;
+			for (int i = 0; i < a.length; i++) {
+				if (s == a[i]) {
 					count++;
 				}
 			}
-			//Step 3.Prinnting Occurence
-			//System.out.println(s+" occurs "+count+" times ");
-			//Step 4.Printing Unique and Duplicates
-			if(count>1)
-				System.out.println(s+" is duplicated repeated "+count+ " times");
+			// Step 3.Prinnting Occurence
+			// System.out.println(s+" occurs "+count+" times ");
+			// Step 4.Printing Unique and Duplicates
+			if (count > 1)
+				System.out.println(s + " is duplicated repeated " + count + " times");
 			else
-				System.out.println(s+" is unique");
-				
+				System.out.println(s + " is unique");
+
 		}
 	}
 

d18e3751cc2c07d7516dfe3d3c9e96c8dcd70296
itsarpan8790 <arpank6@gmail.com>
2023-12-23 12:26:56 +0530

    edits for Com.SianKred/src/test/java/LearnStringProg/ReverseStringExtractNo_08Dec.java

--- Com.SianKred/src/test/java/LearnStringProg/ReverseStringExtractNo_08Dec.java
+++ Com.SianKred/src/test/java/LearnStringProg/ReverseStringExtractNo_08Dec.java
@@ -1,18 +1,31 @@
 package LearnStringProg;
 
+import org.testng.annotations.Test;
+
 public class ReverseStringExtractNo_08Dec {
-	public static void main(String[] args) {
+
+	@Test
+	public void reverse() {
 		String s = "Aabb1235dd45ee1ff44";
 
-		String[] st = s.split("[A-Z a-z]");
-		
-		for (int i = st.length-1; i >=0 ; i--) 
-		{
-			System.out.print(st[i]+" ");
-			
+		String[] st = s.split("[A-Za-z]");
+
+		for (int i = st.length - 1; i >= 0; i--) {
+			System.out.print(st[i] + " ");
+
 		}
-		
 
 	}
 
+	@Test
+	public void reverseExp() {
+		String s = "Aabb1235dd45ee1ff44";
+
+		String[] st = s.split("[A-Za-z]");
+
+		for (int i = 0; i < st.length; i++) {
+			System.out.print(st[i]+" ");
+
+		}
+	}
 }
